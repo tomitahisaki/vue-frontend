f7f49640a6ad8eeed9a1a8cd2131b514bd204134
hisaki <alltimelow2011@gmail.com>
2023-12-25 17:21:30 +0900

    基本理解とSingle File Componentの理解

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,63 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+<script>
+export default {
+  data() {
+    return {
+      title: "Vue3 ホーム画面です",
+      message: "チュートリアル",
+      isRed: true,
+      input: {
+        firstName: "",
+        lastName: "",
+        isMember: true,
+      },
+      users: [
+        {
+          firstName: 'John',
+          lastName: 'Smith',
+          isMember: true,
+        },
+        {
+          firstName: 'Taro',
+          lastName: 'Shinjuku',
+          isMember: false,
+        },
+        {
+          firstName: 'Hanako',
+          lastName: 'Shibuya',
+          isMember: true,
+        },
+      ],
+    }
+  },
+  methods: {
+    addUser() {
+      this.users.push(this.input)
+      this.input = {
+        firstName: "",
+        lastName: "",
+        isMember: true,
+      }
+    },
+  },
+}
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h1 :title="message" :class="{ red: isRed}">{{ title }}</h1>
+  <input type="text" v-model="input.firstName" />
+  <input type="text" v-model="input.lastName" />
+  <input type="checkbox" v-model="input.isMember" />
+  <button v-on:click="addUser">ユーザー追加</button>
+  <h2>ユーザーデータ</h2>
+  <div v-for="user in users">
+    <p>Name: {{ user.firstName + " " + user.lastName }}</p>
+    <p v-if="user.isMember">メンバーです</p>
+    <p v-else>メンバーではありません</p>
+  </div>
 </template>
+
+<style>
+.red {
+  color: red;
+}
+</style>
